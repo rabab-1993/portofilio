dba8fbbd73c2c9cb972bd1c6a7d3e7819748ac78
rabab-1993 <rabab.abdulaziz.elq.js@tuwaiq.edu.sa>
2022-08-24 16:02:22 +0300

    update style

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,21 +1,21 @@
-import { Menu } from "antd";
 import React, { useState } from "react";
+import { Menu } from "antd";
 import { Link } from "react-router-dom";
 // import'antd/dist/antd.less';
-import 'antd/dist/antd.css';
+import "antd/dist/antd.css";
 import "./style.css";
 const Nav = () => {
   const items = [
     {
-      label: (<Link to={"/"}>About Me</Link>),
+      label: <Link to={"/"}>About Me</Link>,
       key: "about",
     },
     {
-      label: (<Link to={"/projects"}>Projects</Link>),
+      label: <Link to={"/projects"}>Projects</Link>,
       key: "projects",
     },
     {
-      label: (<Link to={"/contact"}>Contact Me</Link>),
+      label: <Link to={"/contact"}>Contact Me</Link>,
       key: "contact",
     },
   ];
@@ -26,14 +26,20 @@ const Nav = () => {
     setCurrent(e.key);
   };
   return (
-    <Menu
-      className="nav-bar"
-      onClick={onClick}
-      selectedKeys={[current]}
-      mode="horizontal"
-      items={items}
-    />
-  );
+    
+    <>
+      {/* <div style={{ flex: "0 0 50px", height: 20}} /> */}
+        <Menu
+          // style={{ flex: "auto", minWidth: 0, }}
+          className="nav-bar"
+          onClick={onClick}
+          selectedKeys={[current]}
+          mode="horizontal"
+          items={items}
+          />
+      {/* <div style={{ flex: "0 0 50px", height: 20}} /> */}
+          </>
+  )
 };
 
 export default Nav;
